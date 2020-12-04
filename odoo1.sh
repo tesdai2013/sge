6cacc68b5f549246f8c62030e1c604942c969fd5
tesdai2013 <75480710+tesdai2013@users.noreply.github.com>
2020-12-04 14:07:13 +0100

    Update odoo1.sh

--- odoo1.sh
+++ odoo1.sh
@@ -4,5 +4,5 @@ mkdir odoo
 cd odoo
 mkdir src
 cd ..
-docker run -d -p 8080:8080 --name servidorodoo -v /home/tesdai/odoo/src:/opt/odoo/src debian:buster bash -c "while true;do sleep 10;done"
+docker run -d -p 8069:8069 --name servidorodoo -v /home/tesdai/odoo/src:/opt/odoo/src debian:buster bash -c "while true;do sleep 10;done"
 
